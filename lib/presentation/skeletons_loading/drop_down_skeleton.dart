09b30c2357a5f0c1830f4ca7e458f83bbd44f6f5
nehalgamal093 <nehalgamal093@gmail.com>
2024-02-22 13:47:32 +0200

    improve categories skeleton

--- lib/presentation/skeletons_loading/drop_down_skeleton.dart
+++ lib/presentation/skeletons_loading/drop_down_skeleton.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 Widget dropDownSkeleton() {
-  return Column(
-    children: [
-      Container(
-        width: 500,
-        height: 50,
-        decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 244, 229, 247),
-            borderRadius: BorderRadius.all(Radius.circular(5))),
-      ),
-      const SizedBox(height: 5),
-    ],
-  );
+  return ListView.builder(
+      shrinkWrap: true,
+      itemCount: 5,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: 70,
+            height: 70,
+            decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 244, 229, 247),
+                borderRadius: BorderRadius.all(Radius.circular(5))),
+          ),
+        );
+      });
 }
